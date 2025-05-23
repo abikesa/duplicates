9fb3aac7408e0ec84fa3c3f04f2104fbf57af91b
abikesa <144293697+abikesa@users.noreply.github.com>
2025-05-23 14:56:33 -0400

    badass

--- duplicates.sh
+++ duplicates.sh
@@ -1,1 +1,4 @@
 curl -s https://raw.githubusercontent.com/abikesa/duplicates/refs/heads/main/duplicates.py | python3
+
+curl -O https://raw.githubusercontent.com/abikesa/duplicates/refs/heads/main/duplicates.py
+python3 duplicates.py .
