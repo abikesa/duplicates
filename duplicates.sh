55dc8d116661f8d217e1cd323c6986e650836aca
abikesa <144293697+abikesa@users.noreply.github.com>
2025-06-02 20:33:40 -0400

    bellissimo

--- duplicates.sh
+++ duplicates.sh
@@ -1,4 +1,1 @@
-curl -s https://raw.githubusercontent.com/abikesa/duplicates/refs/heads/main/duplicates.py | python3
-
-curl -O https://raw.githubusercontent.com/abikesa/duplicates/refs/heads/main/duplicates.py
-python3 duplicates.py .
+curl -s https://raw.githubusercontent.com/abikesa/duplicates/refs/heads/main/duplicates.py | python3 - .
